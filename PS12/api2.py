240547f0734c6ce23d92be4824e2b7173211afd7
RaspberryPi <abhi.anu003@gmail.com>
2021-10-14 11:34:57 +0530

    download db

--- PS12/api2.py
+++ PS12/api2.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify,send_file
 from werkzeug import secure_filename
 import os
 import utilities, queries
@@ -100,10 +100,27 @@ def upload_thanalist_fxn():
       utilities.addthanaListData(path_of_csv)
       return "Thana File Saved and Loaded to Database Successfully"
 
-
+# ############################### Queries ##################################
 @app.route('/query/1', methods = ['GET'])
 def query_1():
-   query = "SELECT Name, Aadhar_Number, AC_Number from BankData BD, FIR F WHERE BD.Mobile_Number=F.Mobile_No_of_Complaint"
+   query = "SELECT * FROM CallData ORDER BY duration DESC"
+   result = queries.runQuery(query)
+   response = {'result':result}
+   return jsonify(response)
+
+@app.route('/query/2/', methods = ['GET'])
+def query_2():
+   # Parsing the Headers
+   since = request.args.get('since')
+   till = request.args.get('till')
+   query = f"SELECT * FROM CallData WHERE start_time > {till} AND start_time < {since};"
+   result = queries.runQuery(query)
+   response = {'result':result}
+   return jsonify(response)
+
+@app.route('/query/3/', methods = ['GET'])
+def query_3():
+   query = f"SELECT * FROM CallData ORDER BY duration DESC LIMIT 10"
    result = queries.runQuery(query)
    response = {'result':result}
    return jsonify(response)
@@ -126,6 +143,17 @@ def deleteloaded():
    os.remove("/home/pi/Desktop/AFKZenCoders/PS12/CDRdata.db")
    return jsonify({'CSV files':csv_files})
 
+# Download API
+@app.route("/downloadfile/<filename>", methods = ['GET'])
+def download_file(filename):
+    return render_template('download.html',value=filename)
+
+
+@app.route('/return-files/<filename>')
+def return_files_tut(filename):
+    file_path = "/home/pi/Desktop/AFKZenCoders/PS12/CDRdata.db"
+    #print(file_path)
+    return send_file(file_path, as_attachment=True, attachment_filename='')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port = 1313,debug = True)
